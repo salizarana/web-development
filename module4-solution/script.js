5c80a252256b139f0f70d3cd8c5db6568d0df5a6
Saliza Rana Magar <salizarana@gmail.com>
2020-08-10 12:40:53 +0545

    Add name.

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jimi"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
